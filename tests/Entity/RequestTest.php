d3cacd5f132834be8019ec6ee539260129be485c
Vadim Grudev <dimogrudev98@gmail.com>
2026-05-10 21:38:24 +0500

    Add unit tests for Request value object

---
+++ tests/Entity/RequestTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace WebSocket\Test\Entity;
+
+use PHPUnit\Framework\TestCase;
+use WebSocket\Entity\Request;
+
+class RequestTest extends TestCase
+{
+    public function testRequestStoresDataCorrectly(): void
+    {
+        $headers = [
+            'host'      => 'localhost',
+            'upgrade'   => 'websocket'
+        ];
+        $params  = [
+            'foo'       => 'bar'
+        ];
+        $cookies = [
+            'session'   => 'abc'
+        ];
+
+        $request = new Request('/chat', $headers, $params, $cookies);
+
+        $this->assertSame('/chat', $request->path);
+        $this->assertSame('websocket', $request->header('Upgrade'));
+        $this->assertSame('bar', $request->query('foo'));
+        $this->assertSame('abc', $request->cookie('session'));
+    }
+
+    public function testHeaderIsCaseInsensitive(): void
+    {
+        $request = new Request('/', ['foo' => 'bar']);
+        $this->assertSame('bar', $request->header('FOO'));
+    }
+}
