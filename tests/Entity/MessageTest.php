aa4a7ffe3dedc14e7cbf8e6d1561c5c571fb5bdb
Vadim Grudev <dimogrudev98@gmail.com>
2026-05-10 19:14:09 +0500

    Add unit tests for Message entity

---
+++ tests/Entity/MessageTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace WebSocket\Test\Entity;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use WebSocket\Entity\Message;
+
+class MessageTest extends TestCase
+{
+    public static function lengthProvider(): array
+    {
+        return [
+            'multibyte text'   => ['здравейте', false, 9],
+            'emoji text'       => ['👋', false, 1],
+            'binary emoji'     => ['👋', true, 4],
+            'empty string'     => ['', false, 0],
+            'standard latin'   => ['hello', false, 5],
+            'binary latin'     => ['hello', true, 5],
+        ];
+    }
+
+    #[DataProvider('lengthProvider')]
+    public function testLengthCalculation(string $payload, bool $isBinary, int $expectedLength): void
+    {
+        $msg = new Message($payload, $isBinary);
+        $this->assertEquals($expectedLength, $msg->length);
+    }
+
+    public function testToStringReturnsPayload(): void
+    {
+        $msg = new Message('hello');
+        $this->assertEquals('hello', (string)$msg);
+    }
+}
